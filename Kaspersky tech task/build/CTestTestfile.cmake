f78d6f22ef1c42b4de7d0ac168817a3a69f12b26
Pavel <144141702+paulprishol@users.noreply.github.com>
2025-11-05 01:44:53 +0300

    Delete Kaspersky tech task directory

--- Kaspersky tech task/build/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: D:/kasperskiy task
-# Build directory: D:/kasperskiy task/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-add_test(ScannerTest "D:/kasperskiy task/build/scanner_tests.exe")
-set_tests_properties(ScannerTest PROPERTIES  _BACKTRACE_TRIPLES "D:/kasperskiy task/CMakeLists.txt;39;add_test;D:/kasperskiy task/CMakeLists.txt;0;")
